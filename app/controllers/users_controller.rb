7f8bfde45ae68912fe91afef60e10c73d8dea371
Kazuki Shimizu <s.k24310723@gmail.com>
2019-10-19 14:45:45 +0900

    Strong parametersの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(@user)
+    else
+      render 'new'
+    end
+  end
+
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  # Strong parametersについてのメソッド
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
