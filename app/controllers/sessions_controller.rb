326b23a1fd64ee028cff6ce2731e5e1cc1d2e06a
Kazuki Shimizu <s.k24310723@gmail.com>
2019-10-27 17:01:38 +0900

    Add a basic login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,18 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user &.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
