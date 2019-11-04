8e8dbf14566fab82a2aa5f2b6163db7d73321193
Kazuki Shimizu <s.k24310723@gmail.com>
2019-11-04 10:25:59 +0900

    Task一覧表と投稿ページの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,24 @@
 class TasksController < ApplicationController
   before_action :not_logged_in_user?, only: [:new, :create, :destroy]
 
-  def new; end
+  def new
+    @task = Task.new
+  end
 
-  def create; end
+  def create
+    @task = current_user.tasks.build(micropost_params)
+    if @task.save
+      redirect_to current_user
+    else
+      render new_task_path
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def micropost_params
+    params.require(:task).permit(:content)
+  end
 end
